99792aba80f0e3c9ce0a12a68f2ae698ca8be0bf
nikeee13 <nikeee@outlook.com>
2013-12-09 14:56:17 +0100

    Update .gitignore, more compability

---
+++ src/WOL/PhysicalAddress.cs
@@ -0,0 +1,45 @@
+﻿#if SILVERLIGHT
+
+// Other Frameworks have a System.Net.Networkinformation.PhysicalAddress
+// No PhysicalAddress class in Silverlight, so we have to make our own.
+
+namespace System.Net
+{
+    public class PhysicalAddress
+    {
+        public static readonly byte[] None = new byte[0];
+
+        private byte[] _address;
+
+        public PhysicalAddress(byte[] address)
+        {
+            if (address == null)
+                _address = new byte[0];
+            _address = address;
+        }
+
+        public byte[] GetAddressBytes()
+        {
+            byte[] arr = new byte[_address.Length];
+            Buffer.BlockCopy(_address, 0, arr, 0, _address.Length);
+            return arr;
+        }
+
+        public override bool Equals(object other)
+        {
+            var pa = other as PhysicalAddress;
+            if (pa == null)
+                return false;
+
+            if (_address.Length != pa._address.Length)
+                return false;
+
+            for (int i = 0; i < _address.Length; i++)
+                if (_address[i] != pa._address[i])
+                    return false;
+            return true;
+        }
+    }
+}
+
+#endif
---
+++ src/WOL/UdpClient.cs
@@ -0,0 +1,75 @@
+﻿#if SILVERLIGHT
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace System.Net.Sockets
+{
+    // Silverlight-Compability implementation of the .NET UdpClient
+    // (SL does not have an UdpClient)
+
+    internal class UdpClient : IDisposable
+    {
+        private Socket _socket;
+
+        public UdpClient()
+            : this(AddressFamily.InterNetwork)
+        { }
+
+        public UdpClient(AddressFamily addressFamily)
+        {
+            _socket = new Socket(addressFamily, SocketType.Dgram, ProtocolType.Udp);
+        }
+
+        public void Send(byte[] dgram, int bytes, IPEndPoint endPoint)
+        {
+            CheckDisposed();
+            if (dgram == null)
+                throw new ArgumentNullException("dgram");
+
+            var asc = new SocketAsyncEventArgs();
+            asc.SetBuffer(dgram, 0, dgram.Length);
+            asc.UserToken = _socket;
+            asc.RemoteEndPoint = endPoint;
+            _socket.SendAsync(asc);
+        }
+
+        public void Close()
+        {
+            this.Dispose(true);
+        }
+
+        private bool _disposed;
+        private void CheckDisposed()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(this.GetType().FullName);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+            if (disposing)
+            {
+                if (_socket != null)
+                {
+                    _socket.Shutdown(SocketShutdown.Both);
+                    _socket.Close();
+                    _socket = null;
+                }
+                GC.SuppressFinalize(this);
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+    }
+}
+
+#endif
